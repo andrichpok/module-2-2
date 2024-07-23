478ebbe0669a06b12d3e593bc740fae98d6c5740
Andrei Vanchagov <andreivanchagov@gmail.com>
2024-07-23 16:09:31 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = 8
+second = 52
+third = 21
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
